4eca8a04f7f5868de5643eba0967671666eccf48
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-06-15 23:53:45 +0100

    Initial commit

---
+++ mobolaji_olawale/mobolaji_olawale/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mobolaji_olawale project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobolaji_olawale.settings')
+
+application = get_asgi_application()
